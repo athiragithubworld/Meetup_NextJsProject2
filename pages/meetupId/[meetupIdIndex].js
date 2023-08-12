0427a15cfe4afa3452feca4838d1487738451e3f
athiragithubworld <athirahrms@gmail.com>
2023-08-12 15:36:55 +0530

    meetupIdIndex.js (Task -6 ) Used getStaticProps  , getStaticPaths , fallback

--- pages/meetupId/[meetupIdIndex].js
+++ pages/meetupId/[meetupIdIndex].js
@@ -2,42 +2,43 @@ import React from "react";
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 import { useRouter } from "next/router";
 
+const Dummy_MeetUps = [
+  {
+    id: "m1",
+    title: " A First Meetup ",
+    image:
+      "https://lookahead.com.au/assets/blog/_1200x628_crop_center-center_82_line/meetups.jpg",
+    address: "ABC 5 , South City 78975 ",
+    description: "This is the first meet up!",
+  },
+  {
+    id: "m2",
+    title: " A Second Meetup ",
+    image:
+      "https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.webp",
+    address: "SSSS 5 , South City 78975 ",
+    description: "This is the Second meet up!",
+  },
+  {
+    id: "m3",
+    title: " A Third Meetup ",
+    image:
+      "https://wallpapers.com/images/hd/glamorous-mountain-nature-view-5zvjo2ypc8wic0uj.webp",
+    address: "Santa 9 , North City 5674566 ",
+    description: "This is the Third meet up!",
+  },
+];
+
 const meetupIdIndex = (props) => {
-  const Dummy_MeetUps = [
-    {
-      id: "m1",
-      title: " A First Meetup ",
-      image:
-        "https://lookahead.com.au/assets/blog/_1200x628_crop_center-center_82_line/meetups.jpg",
-      address: "ABC 5 , South City 78975 ",
-      description: "This is the first meet up!",
-    },
-    {
-      id: "m2",
-      title: " A Second Meetup ",
-      image:
-        "https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.webp",
-      address: "SSSS 5 , South City 78975 ",
-      description: "This is the Second meet up!",
-    },
-    {
-      id: "m3",
-      title: " A Third Meetup ",
-      image:
-        "https://wallpapers.com/images/hd/glamorous-mountain-nature-view-5zvjo2ypc8wic0uj.webp",
-      address: "Santa 9 , North City 5674566 ",
-      description: "This is the Third meet up!",
-    },
-  ];
   const router = useRouter();
 
   const meetupId = router.query.meetupIdIndex;
 
-  console.log("id", meetupId);
+  // console.log("id", meetupId);
 
   let teamData = Dummy_MeetUps.find((item) => item.id === meetupId);
 
-  console.log("teamdata", teamData);
+  // console.log("teamdata", teamData);
   return (
     <div>
       {teamData && (
@@ -51,5 +52,48 @@ const meetupIdIndex = (props) => {
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupIdIndex: "m1",
+        },
+      },
+      {
+        params: {
+          meetupIdIndex: "m2",
+        },
+      },
+      {
+        params: {
+          meetupIdIndex: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for single meetup
+
+  const meetupId = context.params.meetupIdIndex;
+
+  let teamData = Dummy_MeetUps.find((item) => item.id === meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: teamData.title,
+        image: teamData.image,
+        address: teamData.address,
+        description: teamData.description,
+      },
+    },
+  };
+}
 
 export default meetupIdIndex;
